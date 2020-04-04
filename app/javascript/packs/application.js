92677d13294b585c857ee3b9e182a9b713f39e2b
fred maurice <fmmadelon@gmail.com>
2020-04-04 00:48:55 -0400

    added post with action text, imagemagick, will_paginate & friendly_id

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,6 @@ import 'bootstrap';
 import './src/application.scss';
 
 global.$ = jQuery;
+
+require("trix")
+require("@rails/actiontext")
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
-  root 'pages#about'
+  root 'posts#index'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
